47dd6125639c17c9fe05416784a9f1ac587cc9a4
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2024-01-15 05:23:33 +0000

    check test cases

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[text,setText]= useState("");
+
+  function whatText(e){
+    setText(`Hello ${e.target.value}!`);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Enter your name:</h1>
+        <input type="text" onChange={whatText}></input>
+        <p>{text}</p>
     </div>
   )
 }
